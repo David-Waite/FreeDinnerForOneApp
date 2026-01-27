0c4f36959e1394409c8a75adf3c81d91df394c50
David Waite <Davidwaite122@gmail.com>
2026-01-27 21:51:10 +1100

    react-native-reanimated fix

--- plugins/withIosRelaxedIncludes.js
+++ plugins/withIosRelaxedIncludes.js
@@ -1,63 +1,36 @@
-const { withDangerousMod } = require("@expo/config-plugins");
-const fs = require("fs");
-const path = require("path");
+const { withPodfile } = require("expo/config-plugins");
 
 const withIosRelaxedIncludes = (config) => {
-  return withDangerousMod(config, [
-    "ios",
-    async (config) => {
-      const file = path.join(config.modRequest.platformProjectRoot, "Podfile");
-      const contents = await fs.promises.readFile(file, "utf8");
+  return withPodfile(config, (config) => {
+    const podfileContents = config.modResults.contents;
 
-      // Robust fix for Firebase + Static Frameworks missing RN headers
-      // We add multiple search paths to ensure RCTBridgeModule.h is found.
-      const fixScript = `
-    installer.pods_project.targets.each do |target|
-      target.build_configurations.each do |config|
-        config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
-        
-        # Add comprehensive search paths for React Native headers
-        config.build_settings['HEADER_SEARCH_PATHS'] ||= '$(inherited)'
-        config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/React-Core"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/React-bridging/react/bridging"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/React-cxxreact"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/React-callinvoker"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/React-runtimeexecutor"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/Yoga"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/React-hermes"'
+    // This setting tells Xcode to allow Frameworks (like Firebase) to import
+    // non-modular headers (like React Native).
+    const buildSetting = `
+      installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+          config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+        end
       end
-    end
+    `;
+
+    // Inject this setting into the existing post_install block
+    if (podfileContents.includes("post_install do |installer|")) {
+      config.modResults.contents = podfileContents.replace(
+        "post_install do |installer|",
+        `post_install do |installer|\n${buildSetting}`,
+      );
+    } else {
+      // Fallback: If for some reason there isn't one, add it to the end
+      config.modResults.contents += `
+        post_install do |installer|
+          ${buildSetting}
+        end
       `;
+    }
 
-      // Prevent duplicate injection
-      if (
-        contents.includes(
-          "CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES",
-        )
-      ) {
-        return config;
-      }
-
-      let newContents = contents;
-      // Inject inside the existing post_install block
-      if (contents.includes("post_install do |installer|")) {
-        newContents = contents.replace(
-          "post_install do |installer|",
-          `post_install do |installer|
-${fixScript}`,
-        );
-      } else {
-        newContents += `
-          post_install do |installer|
-            ${fixScript}
-          end
-        `;
-      }
-
-      await fs.promises.writeFile(file, newContents);
-      return config;
-    },
-  ]);
+    return config;
+  });
 };
 
 module.exports = withIosRelaxedIncludes;
