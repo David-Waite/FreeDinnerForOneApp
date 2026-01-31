2831e08f697fb3bc6488f595b1e6b9b034f968f3
David Waite <Davidwaite122@gmail.com>
2026-01-31 16:46:22 +1100

    Name change (and hopfully crash fix)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import "react-native-get-random-values";
 import { useEffect, useState } from "react";
 import { Stack, useRouter, useSegments } from "expo-router";
 import { onAuthStateChanged, User } from "firebase/auth";
